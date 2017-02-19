bd3b3e170870ebe50062a00c4af90d21cb29ae22
DV <dvislearning@gmail.com>
2017-02-18 20:25:12 -0800

    finish main functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def set_env_variables
+  	FlickRaw.api_key = ENV["api_key"]
+  	FlickRaw.shared_secret = ENV["shared_secret"]
+  end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,29 @@
 class StaticPagesController < ApplicationController
   def home
+  	@user_id = params[:user_id]
+  	unless @user_id.nil? || @user_id.empty?
+  		@photos = user_photos(@user_id, photo_count = 12)
+  	else
+  		if params[:commit] ==  "Search"
+  			@photos = []
+  		end
+  	 end
   end
+
+  private
+
+	def user_photos(user_id, photo_count = 12)
+		begin
+			set_env_variables
+			photo_ids = []
+			from_flickr = flickr.photos.search(:user_id => user_id).to_a
+			from_flickr.each do |photo|
+				photo_ids << photo.id
+			end
+			return photo_ids.in_groups_of(2)
+		rescue Exception
+			return []
+		end
+	end
 end
+
